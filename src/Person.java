177ea6d5ed10f648661c5da3cb0656953827b313
moldenburger <maritoldenburger@hotmail.com>
2025-01-06 14:47:10 +0100

    Added classes and attributes.

---
+++ src/Person.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List<Person> siblings = new ArrayList<>();
+    private List<Person> children = new ArrayList<>();
+    private List<Person> pets = new ArrayList<>();
+
+    public Person (String name, String lastName, String sex, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public Person(String name, String middleName, String lastName, String sex, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.age = age;
+    }
+}
---
+++ src/Pet.java
@@ -0,0 +1,12 @@
+public class Pet {
+    private String name;
+    private int age;
+    private String species;
+    private Person owner;
+
+    public Pet(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+}
